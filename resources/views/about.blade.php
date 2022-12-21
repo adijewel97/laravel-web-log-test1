65ab065c6b12eb6cc611914d7c9b707d41514b02
AdiJewel <adijewel97@yahoo.com>
2022-12-21 12:25:31 +0700

    laravel web log commit pertama

---
+++ resources/views/about.blade.php
@@ -0,0 +1,5 @@
+@extends('layouts.main')
+
+@section('container')
+<h1>ini About euy</h1>
+@endsection
---
+++ routes/web.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+use Illuminate\Support\Facades\Log;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('/', function () {
+    Log::info('Sukses : test info mang adis.');
+    // Log::warning('Something could be going wrong.');
+    // Log::error('Something is really going wrong.');
+
+    return view('home', [
+        'title' => 'Home'
+    ]);
+});
+
+
+Route::get('/about', function () {
+    return view('about', [
+        'title' => 'About'
+    ]);
+});
+
+Route::get('/blog', function () {
+    return view('blog', [
+        'title' => 'Blog'
+    ]);
+});
